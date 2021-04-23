2fd9d255d6ac8d5aa6c0a3e101aea38ac2fbbb58
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-04-23 12:51:29 -0500

    Done.

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -34,7 +34,7 @@ export default class MovieCard extends Component {
     return (
       <div className="movie-card">
         {/* which component should receive which props? */}
-        <CardFront poster={this.props.poster}/>
+        <CardFront poster={posterMap[this.props.poster]}/>
         <CardBack title={this.props.title} genres={this.props.genres} IMDBRating={this.props.IMDBRating}/>
       </div>
     )
